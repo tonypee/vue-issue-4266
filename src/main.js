6cf019a6e35842e577195b1f6714c835483ee0bc
Tony Polinelli <tonypolinelli@gmail.com>
2016-11-22 00:53:56 +1100

    initial commit

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import Vue from 'vue'
+import App from './App'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  template: '<App/>',
+  components: { App }
+})
